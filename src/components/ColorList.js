9de235e5a24b1c8efa53d735d4117d910abc33b4
Jackson-Kahono <jackson.kahono@student.moringaschool.com>
2022-10-04 09:21:53 +0300

    completed assignmet

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+function ColorItem(props) {
+  return <li style={{ color: props.color }}>{props.color}</li>;
+}
+
 function ColorList() {
   const colors = [
     "firebrick",
@@ -9,16 +13,14 @@ function ColorList() {
     "hotpink",
   ];
 
-  const colorsList = colors.map((color) => (
-    <li key={color} style={{ color }}>
-      {color}
-    </li>
-  ));
+  const colorElements = colors.map((color) => {
+    return <ColorItem key={color} color={color} />;
+  });
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        {colorsList}
+        {colorElements}
       </ol>
     </div>
   );
